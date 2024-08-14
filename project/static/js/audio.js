dfcd261b5332bc7d75b8ae209fbca7edbe7a0f72
Ryujinwoo1 <goodtim123@naver.com>
2024-08-15 06:12:52 +0900

    html_v2

--- project/static/js/audio.js
+++ project/static/js/audio.js
@@ -1,12 +1,22 @@
 const recordButton = document.getElementById('recordButton');
 const audio = document.getElementById('audioPlayer');
+const backgroundVideo = document.getElementById('backgroundVideo');
+const recordIcon = document.getElementById('recordIcon');
 
 let mediaRecorder;
+const originalVideoSource = '/static/video/Idle_2.mp4'; // 원래 배경 비디오
+const randomVideos = [
+    '/static/video/Speaking_gesture.mp4',
+    '/static/video/Speaking_gesture1.mp4',
+    '/static/video/Speaking_gesture2.mp4',
+    '/static/video/Speaking_gesture3.mp4',
+    '/static/video/Speaking.mp4'
+];
 
 recordButton.addEventListener('click', () => {
     if (mediaRecorder && mediaRecorder.state == "recording") {
         mediaRecorder.stop();
-        recordButton.textContent = "녹음 시작";
+        recordIcon.className = 'fas fa-microphone';
     } else {
         navigator.mediaDevices.getUserMedia({ audio: true })
         .then(stream => {
@@ -36,17 +46,34 @@ recordButton.addEventListener('click', () => {
                         const duration = audio.duration;
                         console.log('음성 파일의 총 재생 시간:', duration, '초');
                     });
+
+                    // 녹음이 시작되면 랜덤 비디오로 배경 변경
+                    const randomIndex = Math.floor(Math.random() * randomVideos.length);
+                    backgroundVideo.src = randomVideos[randomIndex];
+                    backgroundVideo.load();
+
+                    
                     audio.src = url;
                     audio.autoplay = true;
+
+                    audio.onended = () => {
+                        // 오디오가 종료되면 원래 비디오로 돌아가기
+                        backgroundVideo.src = originalVideoSource;
+                        backgroundVideo.load();
+                    };
+
                 })
                 .catch(error => {
                     console.error('Error:', error);
                 });
                   
             };
+
+            
+
             
             mediaRecorder.start();
-            recordButton.textContent = "녹음 중지";
+            recordIcon.className = 'fas fa-stop';
         })
         .catch(err => {
             console.error('Error accessing media devices.', err);
